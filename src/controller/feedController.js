18326ebabed33f9dafc4af7147feee7ab54941cf
Anshulranjan845 <anshulgulati177@gmail.com>
2025-09-19 11:00:33 +0530

    skip and limit api

--- src/controller/feedController.js
+++ src/controller/feedController.js
@@ -2,7 +2,11 @@ const ConnectionSchema = require("../models/request.model");
 const User = require("../models/user.model");
 
 exports.getFeed = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
   const user = req.user;
+  const USER_SELECTED_FIELD =
+    "firstName lastName email profilePicture bio gender skills";
   try {
     const connectedUser = await ConnectionSchema.find({
       $or: [{ sender: user.id }, { recipient: user.id }],
@@ -20,7 +24,10 @@ exports.getFeed = async (req, res) => {
         { _id: { $nin: Array.from(hideUser) } },
         { _id: { $ne: user.id } },
       ],
-    });
+    })
+      .select(USER_SELECTED_FIELD)
+      .skip((page - 1) * limit)
+      .limit(limit);
 
     res.json({
       success: true,
